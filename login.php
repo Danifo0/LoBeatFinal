e0b225edcc3e2326d0a4ffc6ba6f3c5a6f57a4a9
Danifo0 <75387664+Danifo0@users.noreply.github.com>
2023-03-09 16:26:35 +0100

    Login y Registro html css

---
+++ login.php
@@ -0,0 +1,37 @@
+<html lang="en" >
+<head>
+  <title>Login</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link href="css/reg_log.css" rel="stylesheet">
+</head>
+<body>
+<div class= "fondo">
+  <div class="login-box">
+   <img class ="logo"src= "img/LogoLB.png">
+    <h2>Login</h2>
+    <form>
+      <div class="user-box">
+        <input type="text" name="" required="">
+        <label>Usuario/email</label>
+      </div>
+      <div class="user-box">
+        <input type="password" name="" required="">
+        <label>Contraseña</label>
+      </div>
+      <p> Si no tienes una cuenta &nbsp;<a class = "noreg" href = "register.php">Registrate </a></p>
+        <br>
+     
+      <a class = "sig" href="#">
+      <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+         Siguiente</a>
+      </a>
+    </form>
+  </div>
+</div>
+</body>
+
+</html>
---
+++ register.php
@@ -0,0 +1,45 @@
+<html lang="en">
+<head>
+  <title>Register</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link href="css/reg_log.css" rel="stylesheet">
+</head>
+<body>
+<div class= fondo>
+  <div class="login-box">
+   <img class ="logo"src= "img/LogoLB.png">
+    <h2>Sing up</h2>
+    <form>
+      <div class="user-box">
+        <input type="text" name="" required="">
+        <label>Usuario</label>
+      </div>
+      <div class="user-box">
+        <input type="text" name="" required="">
+        <label>Email</label>
+      </div>
+      <div class="user-box">
+        <input type="password" name="" required="">
+        <label>Contraseña</label>
+      </div>
+      <div class="user-box">
+        <input type="text" name="" required="">
+        <label>Tipo de musica favorita</label>
+      </div>
+      <div class="user-box">
+        <input type="text" name="" required="">
+        <label>Cancion preferida</label>
+      </div>
+      <a class="sig"href="login.php">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Siguiente
+      </a>
+    </form>
+  </div>
+</div>
+</body>
+</html>
